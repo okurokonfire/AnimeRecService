bee5f02a67c178456df3adbd841e837d1508514e
Denis Rakhmanov <denisrakhmanov@gmail.com>
2020-11-25 17:17:05 +0300

    some changes

--- scala/src/main/scala/processData.scala
+++ scala/src/main/scala/processData.scala
@@ -8,16 +8,43 @@ import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.streaming.Trigger
-import java.sql.{Connection, DriverManager, ResultSet}
+import java.sql.{Connection, DriverManager, ResultSet, Statement}
 
 import anilist.recsystem.Utils._
 
 object ProcessData {
     case class MediaTag (id: Int, name: String, category: String)
     case class MediaStudio (id: Int, name: String, isMain: Boolean)
+    case class MediaStaff (id: Int, firstName: String, lastName: String, fullName: String, nativeName: String, role: String)
+
+    def updateMedia() = {
+        ???
+    }
+
+    def insertMedia() = {
+        ???
+    }
+
+    def getMediaTypeId(stmt: Statement, mediaName: String): Int = {
+        val query = s"select mediatypeid from public.tmediatype where name = '${mediaName}';"
+        val rs = stmt.executeQuery(query)
+        rs.next match {
+            case true => rs.getInt(1)
+            case false => {
+                val q = s"insert into public.tmediatype(name) select '${mediaName}';"
+                val rs0 = stmt.executeUpdate(q)
+                val rs1 = stmt.executeQuery(query)
+                if (rs1.next) {
+                    rs1.getInt(1)
+                } else {
+                    throw new Exception(s"something went wrong while inserting mediaType = $mediaName")
+                }
+            }
+        }
+    }
 
     def getDate(date: play.api.libs.json.JsValue) = {
-        val (year,month,day) = (isnull(date("year"),1900),isnull(date("month"),1),isnull(date("day"),1)) 
+        val (year,month,day) = (anilist.recsystem.Utils.isnull(date("year"),1900),anilist.recsystem.Utils.isnull(date("month"),1),anilist.recsystem.Utils.isnull(date("day"),1)) 
         val newDate = Try(java.time.LocalDate.of(year,month,day)).getOrElse(java.time.LocalDate.of(1900,1,1))
         newDate.toString
     }
@@ -32,7 +59,8 @@ object ProcessData {
             val json: JsValue = Json.parse(input)
             val media = json \ "data" \ "Media"
             val anilistMediaID = media("id").toString.toInt
-            val mediaType = media("type").toString
+            val mediaType = media("type").toString.replace("\"","")
+            val mediaTypeId = getMediaTypeId(stmt,mediaType)
             val title = media("title")
             val romajiTitle = title("romaji").toString
             val englishTitle = title("english").toString
@@ -60,6 +88,17 @@ object ProcessData {
                 MediaStudio(line("node")("id").toString.toInt,line("node")("name").toString,line("isMain").toString.toBoolean)
             })
 
+            val staff = media("staff")("edges").as[JsArray].value.map(line => {
+                MediaStaff(line("node")("id").toString.toInt,line("node")("name")("first").toString,line("node")("name")("last").toString,line("node")("name")("full").toString,line("node")("name")("native").toString,line("role").toString)
+            })
+
+            val rs = stmt.executeQuery(s"select animeid from public.tanime where anilistanimeid = $anilistMediaID;")
+
+            if (rs.next) {
+                updateMedia()
+            } else {
+                insertMedia()
+            }
 
             sql_connection.commit()
         }).getOrElse(throw new Exception(s"Error while processing ${input}"))
